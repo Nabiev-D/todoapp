93eba43a22d78d75a4e5fb8c3b95fe8cd4b11c58
Nabiev-D1 <asteroy9191@gmail.com>
2020-12-30 01:46:40 +0300

    Revert "GIT init"
    This reverts commit f9fada583837a824a4560a55bc26b07696ca76fe.

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from accounts.models import Profile
-from django.contrib import admin
-
-# Register your models here.
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-	list_display = ["user", "birthdate"]
